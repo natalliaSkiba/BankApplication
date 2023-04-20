be1de20f01c8090cfc9d81327f3c629952ea9429
natalliaSkiba <nt1711182@gmail.com>
2023-04-20 18:07:35 +0200

    тест AccountServiceImplTest getAccountById() - прошел

--- src/test/java/com/telran/bankapplication/service/impl/AccountServiceImplTest.java
+++ src/test/java/com/telran/bankapplication/service/impl/AccountServiceImplTest.java
@@ -6,6 +6,7 @@ import com.telran.bankapplication.mapper.AccountMapper;
 import com.telran.bankapplication.repository.AccountRepository;
 import com.telran.bankapplication.service.util.DtoCreator;
 import com.telran.bankapplication.service.util.EntityCreator;
+import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,6 +16,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
 
 @DisplayName("Account service test class")
 @ExtendWith(MockitoExtension.class)
@@ -38,10 +41,16 @@ class AccountServiceImplTest {
         Account account = EntityCreator.getEntity();
         AccountDTO accountDTO = DtoCreator.getDto();
         UUID uuid = UUID.fromString(EntityCreator.UUID_EXAMPLE);
+
         Mockito.when(accountRepository.findAccountById(uuid)).thenReturn(Optional.ofNullable(account));
         Mockito.when(accountMapper.toDTO(account)).thenReturn(accountDTO);
+
+        AccountServiceImpl accountService = new AccountServiceImpl(accountMapper, accountRepository);
+        AccountDTO returnedAccountDTO = accountService.getAccountById(uuid.toString());
+
         Mockito.verify(accountRepository).findAccountById(uuid);
         Mockito.verify(accountMapper).toDTO(account);
 
+        assertEquals(accountDTO.getName(), returnedAccountDTO.getName());
     }
 }
